ad70ab5da6fd7fde99ec113576cabba18aa41df0
ada-u <ada-u@ada-u.com>
2016-10-12 19:55:07 +0900

    Rename layers

--- keyboard/ergodox_ez/keymaps/adachi/keymap.c
+++ keyboard/ergodox_ez/keymaps/adachi/keymap.c
@@ -5,11 +5,11 @@
 #include "debug.h"
 #include "action_layer.h"
 
-#define LAYER_1 0
-#define LAYER_2 1
+#define DEFAULT 0
+#define FUNCTION 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: LAYER_1
+/* Keymap 0: DEFAULT
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  | `~   |           | `~   |   6  |   7  |   8  |   9  |   0  |   -    |
@@ -32,12 +32,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[LAYER_1] = KEYMAP(  // layer 0 : default
+[DEFAULT] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,         KC_2,        KC_3,      KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,   KC_TRNS,
         KC_LCTL,        KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   TO(LAYER_2,1),
+        KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   TO(FUNCTION,1),
         KC_TRNS,        KC_TRNS,      KC_LALT,     KC_LGUI,   KC_TRNS,
                                                                KC_LEFT,       KC_RGHT,
                                                                               KC_TRNS,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,KC_SPC, KC_ENT
     ),
 
-/* Keymap 1: LAYER_2
+/* Keymap 1: FUNCTION
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 |        |
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 // Fnctions
-[LAYER_2] = KEYMAP(
+[FUNCTION] = KEYMAP(
        // left hand
        KC_TRNS,       KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
        KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      KC_SPC, KC_ENT, KC_TRNS,
        // right hand
        KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-       KC_RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+       RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
@@ -139,10 +139,10 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case LAYER_1:
+        case DEFAULT:
             ergodox_right_led_1_on();
             break;
-        case LAYER_2:
+        case FUNCTION:
             ergodox_right_led_2_on();
             break;
         default:
