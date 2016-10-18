cb95e5ce765016744730141fac578ac41098fc9c
ada-u <ada-u@ada-u.com>
2016-10-18 18:40:33 +0900

    Fix LANG Keycodes

--- keyboards/ergodox/keymaps/adachi/keymap.c
+++ keyboards/ergodox/keymaps/adachi/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,   KC_TRNS,
         KC_LCTL,        KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   TG(FN),
-        KC_TRNS,        KC_TRNS,      KC_LALT,     KC_LGUI,   KC_LANG1,
+        KC_TRNS,        KC_TRNS,      KC_LALT,     KC_LGUI,   KC_LANG2,
                                                                KC_LEFT,       KC_RGHT,
                                                                               KC_TRNS,
                                                                KC_SPC,KC_LSFT,KC_TRNS,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_EQL, KC_RCTL,KC_TRNS,KC_TRNS,          KC_BSPC,
         KC_LBRC,        KC_RBRC,
         KC_TRNS,
-        KC_TRNS,KC_LANG2, KC_ENT
+        KC_TRNS,KC_LANG1, KC_ENT
     ),
 
 /* Keymap 1: FN
